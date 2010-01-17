ab2530f8c63ef9fb4b0eecf56a5c72d8b27884a9
Matthew Mongeau <halogenandtoast@Matthew-Mongeaus-iMac.local>
2010-01-16 22:59:51 -0500

    Add display for other notification types

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -6,4 +6,12 @@ module DashboardHelper
   def accepted_notification_display(notification)
     "#{notification.listing.user.username} accepted your offer of <span class='price'>$#{notification.data[:amount]}</span>"
   end
+  
+  def declined_notification_display(notification)
+      "#{notification.listing.user.username} declined your offer of <span class='price'>$#{notification.data[:amount]}</span>"
+  end
+  
+  def countered_notification_display(notification)
+      "#{notification.listing.user.username} countered your offer of <span class='price'>$#{notification.data[:amount]}</span>"
+  end
 end
