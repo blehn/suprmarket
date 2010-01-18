9bc79f513f277296bd7fc4e82a44aa2d0e0055e8
Matthew Mongeau <halogenandtoast@Matthew-Mongeaus-iMac.local>
2010-01-17 20:56:46 -0500

    Runner with more stuff

--- runner.rb
+++ runner.rb
@@ -19,24 +19,28 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 require 'config/environment'
-a = Nokogiri::HTML(open('http://shop.ebay.com/?_from=R40&_trksid=p3902.m38.l1313&_nkw=a&_sacat=See-All-Categories'))
+
 lorem_text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 categories = Category.all
 cities = City.all
 users = User.all
-a.css('.v4lnk').map(&:text).each do |title|
-  willing_to_ship = [true, false].rand
-  shipping_cost = willing_to_ship ? rand(1000) : nil
-  Listing.create({
-    :title => title, 
-    :description => lorem_text, 
-    :user_id => users.rand.id,
-    :category_id => categories.rand.id,
-    :price => rand(1000),
-    :zip_code => cities.rand.zip_code,
-    :condition => ['new', 'used'].rand,
-    :willing_to_ship => willing_to_ship,
-    :shipping_cost => shipping_cost
-  })
+
+1.upto(5) do |i|
+  a = Nokogiri::HTML(open("http://shop.ebay.com/i.html?_trkparms=65%253A12%257C66%253A2%257C39%253A1%257C72%253A2528&_nkw=a&_trksid=p3286.c0.m14&_rdc=1&+pgn=#{i}"))
+  a.css('.v4lnk').map(&:text).each do |title|
+    willing_to_ship = [true, false].rand
+    shipping_cost = willing_to_ship ? rand(1000) : nil
+    Listing.create({
+      :title => title, 
+      :description => lorem_text, 
+      :user_id => users.rand.id,
+      :category_id => categories.rand.id,
+      :price => rand(1000),
+      :zip_code => cities.rand.zip_code,
+      :condition => ['new', 'used'].rand,
+      :willing_to_ship => willing_to_ship,
+      :shipping_cost => shipping_cost
+    })
     
+  end
 end
