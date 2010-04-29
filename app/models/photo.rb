ef516e2c5f0ae42ff5cb530ed688f7e3635e2bed
Matthew Mongeau <matt@toastyapps.com>
2010-04-28 23:19:14 -0400

    Use large image display for grid

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,4 @@
 class Photo < ActiveRecord::Base
   belongs_to :listing, :counter_cache => true
-  has_attached_file :file, :styles => {:display => '560>', :thumb => '160x100>#', :large => '320x200># => '}
+  has_attached_file :file, :styles => {:display => '560>', :thumb => '160x100>#', :large => '320x200>#'}
 end
